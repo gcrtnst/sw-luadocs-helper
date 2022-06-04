e41c21006227069024e9667ebfe4fc0f41ed69e7
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-06-04 15:04:06 +0900

    Add sw_luadocs.flatdoc.loads_elem

--- src/sw_luadocs/flatdoc.py
+++ src/sw_luadocs/flatdoc.py
@@ -20,3 +20,20 @@ class FlatElem:
 
         object.__setattr__(self, "txt", txt)
         object.__setattr__(self, "kind", kind)
+
+
+def loads_elem(s):
+    s = str(s)
+    line_list = s.split(sep="\n")
+
+    if not (line_list[0].startswith("[") and line_list[0].endswith("]")):
+        raise ValueError
+    kind = line_list[0][1:-1]
+
+    line_list_end = len(line_list) - 1
+    while line_list_end >= 1:
+        if line_list[line_list_end] != "":
+            break
+        line_list_end -= 1
+    txt = "\n".join(line_list[1 : line_list_end + 1])
+    return FlatElem(txt=txt, kind=kind)
--- src/sw_luadocs_tests/test_flatdoc.py
+++ src/sw_luadocs_tests/test_flatdoc.py
@@ -23,3 +23,33 @@ class TestDocumentElemPostInit(unittest.TestCase):
     def test_validate_convert_kind(self):
         with self.assertRaises(ValueError):
             sw_luadocs.flatdoc.FlatElem(txt="", kind="invalid")
+
+
+class TestLoadsElem(unittest.TestCase):
+    def test_validate_value_error(self):
+        for s in ["", "[", "]", "\n[head]", "[head]txt"]:
+            with self.assertRaises(ValueError):
+                sw_luadocs.flatdoc.loads_elem(s)
+
+    def test_kind(self):
+        for s, kind in [
+            ("[head]", "head"),
+            ("[body]", "body"),
+            ("[code]", "code"),
+            ("[head]\n[body]", "head"),
+        ]:
+            flatelem = sw_luadocs.flatdoc.loads_elem(s)
+            self.assertEqual(flatelem.kind, kind)
+
+    def test_txt(self):
+        for s, txt in [
+            ("[body]", ""),
+            ("[body]\n", ""),
+            ("[body]\n\n", ""),
+            ("[body]\ntxt", "txt"),
+            ("[body]\n\ntxt", "\ntxt"),
+            ("[body]\n\ntxt\n", "\ntxt"),
+            ("[body]\n\na\n\nb\n\nc\n", "\na\n\nb\n\nc"),
+        ]:
+            flatelem = sw_luadocs.flatdoc.loads_elem(s)
+            self.assertEqual(flatelem.txt, txt)
