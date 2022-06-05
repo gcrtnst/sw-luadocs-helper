2006906f03e4c49f8ce774f63daa1b18f26eabe6
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-06-05 13:04:23 +0900

    Add sw_luadocs.flatdoc.MdlikeParser

--- src/sw_luadocs/flatdoc.py
+++ src/sw_luadocs/flatdoc.py
@@ -1,3 +1,4 @@
+import contextlib
 import dataclasses
 import itertools
 import typing
@@ -76,3 +77,127 @@ def dumps(flatdoc):
     if loads(s) != flatdoc:
         raise ValueError
     return s
+
+
+class MdlikeParser:
+    _linesep = "\n"
+    _headseq = "# "
+    _codeseq = "```"
+
+    def __init__(self, s):
+        s = str(s)
+        line_list = s.split(self._linesep)
+
+        self._line_list = line_list
+        self._line_idx = 0
+
+    def check_eof(self):
+        return self._line_idx >= len(self._line_list)
+
+    def peek(self, *, required=True):
+        if self.check_eof():
+            if required:
+                raise ValueError
+            return None
+        return self._line_list[self._line_idx]
+
+    def next(self):
+        if not self.check_eof():
+            self._line_idx += 1
+
+    def pop(self, *, required=True):
+        line = self.peek(required=required)
+        self.next()
+        return line
+
+    @contextlib.contextmanager
+    def transaction(self):
+        line_idx = self._line_idx
+        try:
+            yield
+        except:
+            self._line_idx = line_idx
+            raise
+
+    def check_blank(self):
+        line = self.peek(required=False)
+        return line == ""
+
+    def check_head(self):
+        line = self.peek(required=False)
+        return line is not None and line.startswith(self._headseq)
+
+    def check_code(self):
+        line = self.peek(required=False)
+        return line == self._codeseq
+
+    def check_body(self):
+        return (
+            not self.check_eof()
+            and not self.check_blank()
+            and not self.check_head()
+            and not self.check_code()
+        )
+
+    def skip_blank(self):
+        with self.transaction():
+            while self.check_blank():
+                self.next()
+
+    def parse_head(self):
+        if not self.check_head():
+            raise ValueError
+
+        with self.transaction():
+            line = self.pop()
+            txt = line[len(self._headseq) :]
+            return FlatElem(txt=txt, kind="head")
+
+    def parse_body(self):
+        if not self.check_body():
+            raise ValueError
+
+        with self.transaction():
+            line_list = []
+            while self.check_body():
+                line = self.pop()
+                line_list.append(line)
+
+            txt = self._linesep.join(line_list)
+            return FlatElem(txt=txt, kind="body")
+
+    def parse_code(self):
+        if not self.check_code():
+            raise ValueError
+
+        with self.transaction():
+            self.next()
+
+            line_list = []
+            while True:
+                line = self.pop()
+                if line == self._codeseq:
+                    break
+                line_list.append(line)
+
+            txt = self._linesep.join(line_list)
+            return FlatElem(txt=txt, kind="code")
+
+    def parse_elem(self):
+        if self.check_head():
+            return self.parse_head()
+        if self.check_body():
+            return self.parse_body()
+        if self.check_code():
+            return self.parse_code()
+        raise ValueError
+
+    def parse(self):
+        with self.transaction():
+            flatdoc = []
+            while True:
+                self.skip_blank()
+                if self.check_eof():
+                    return flatdoc
+                flatelem = self.parse_elem()
+                flatdoc.append(flatelem)
--- src/sw_luadocs_tests/test_flatdoc.py
+++ src/sw_luadocs_tests/test_flatdoc.py
@@ -142,3 +142,414 @@ class TestDumps(unittest.TestCase):
             with self.subTest(flatdoc=input_flatdoc):
                 actual_s = sw_luadocs.flatdoc.dumps(input_flatdoc)
                 self.assertEqual(actual_s, expected_s)
+
+
+class TestMdlikeParserInit(unittest.TestCase):
+    def test_main(self):
+        p = sw_luadocs.flatdoc.MdlikeParser(0)
+        self.assertEqual(p._line_list, ["0"])
+        self.assertEqual(p._line_idx, 0)
+
+
+class TestMdlikeParserCheckEOF(unittest.TestCase):
+    def test_main(self):
+        for input_line_list, input_line_idx, expected_result in [
+            ([""], 0, False),
+            ([""], 1, True),
+            (["", ""], 1, False),
+            (["", ""], 2, True),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_result = p.check_eof()
+                self.assertEqual(actual_result, expected_result)
+
+
+class TestMdlikeParserPeek(unittest.TestCase):
+    def test_error(self):
+        p = sw_luadocs.flatdoc.MdlikeParser("")
+        p._line_list = [""]
+        p._line_idx = 1
+        with self.assertRaises(ValueError):
+            p.peek()
+
+    def test_pass(self):
+        for input_line_list, input_line_idx, input_required, expected_line in [
+            (["a", "b", "c"], 0, True, "a"),
+            (["a", "b", "c"], 1, True, "b"),
+            (["a", "b", "c"], 2, True, "c"),
+            (["a", "b", "c"], 3, False, None),
+        ]:
+            with self.subTest(
+                line_list=input_line_list,
+                line_idx=input_line_idx,
+                required=input_required,
+            ):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_line = p.peek(required=input_required)
+                self.assertEqual(actual_line, expected_line)
+
+
+class TestMdlikeParserNext(unittest.TestCase):
+    def test_main(self):
+        for input_line_list, input_line_idx, expected_line_idx in [
+            ([""], 0, 1),
+            ([""], 1, 1),
+            (["", ""], 1, 2),
+            (["", ""], 2, 2),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                p.next()
+                self.assertEqual(p._line_idx, expected_line_idx)
+
+
+class TestMdlikeParserPop(unittest.TestCase):
+    def test_error(self):
+        p = sw_luadocs.flatdoc.MdlikeParser("")
+        p._line_list = [""]
+        p._line_idx = 1
+        with self.assertRaises(ValueError):
+            p.pop()
+
+    def test_pass(self):
+        for (
+            input_line_list,
+            input_line_idx,
+            input_required,
+            expected_line_idx,
+            expected_line,
+        ) in [
+            (["a", "b", "c"], 0, True, 1, "a"),
+            (["a", "b", "c"], 1, True, 2, "b"),
+            (["a", "b", "c"], 2, True, 3, "c"),
+            (["a", "b", "c"], 3, False, 3, None),
+        ]:
+            with self.subTest(
+                line_list=input_line_list,
+                line_idx=input_line_idx,
+                required=input_required,
+            ):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_line = p.pop(required=input_required)
+                self.assertEqual(p._line_idx, expected_line_idx)
+                self.assertEqual(actual_line, expected_line)
+
+
+class TestMdlikeParserTransaction(unittest.TestCase):
+    def test_pass(self):
+        p = sw_luadocs.flatdoc.MdlikeParser("\n")
+        p._line_idx = 0
+        with p.transaction():
+            p._line_idx = 1
+        self.assertEqual(p._line_idx, 1)
+
+    def test_rollback(self):
+        p = sw_luadocs.flatdoc.MdlikeParser("\n")
+        p._line_idx = 0
+        with self.assertRaises(ValueError):
+            with p.transaction():
+                p._line_idx = 1
+                raise ValueError
+        self.assertEqual(p._line_idx, 0)
+
+    def test_nested(self):
+        p = sw_luadocs.flatdoc.MdlikeParser("\n")
+        p._line_idx = 0
+        with p.transaction():
+            p._line_idx = 1
+            with self.assertRaises(ValueError):
+                with p.transaction():
+                    p._line_idx = 2
+                    raise ValueError
+            self.assertEqual(p._line_idx, 1)
+        self.assertEqual(p._line_idx, 1)
+
+
+class TestMdlikeParserCheckBlank(unittest.TestCase):
+    def test_main(self):
+        for input_line_list, input_line_idx, expected_result in [
+            (["a"], 0, False),
+            ([""], 0, True),
+            ([""], 1, False),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_result = p.check_blank()
+                self.assertEqual(p._line_idx, input_line_idx)
+                self.assertEqual(actual_result, expected_result)
+
+
+class TestMdlikeParserCheckHead(unittest.TestCase):
+    def test_main(self):
+        for input_line_list, input_line_idx, expected_result in [
+            (["# "], 0, True),
+            (["# head"], 0, True),
+            (["head"], 0, False),
+            (["# "], 1, False),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_result = p.check_head()
+                self.assertEqual(p._line_idx, input_line_idx)
+                self.assertEqual(actual_result, expected_result)
+
+
+class TestMdlikeParserCheckCode(unittest.TestCase):
+    def test_main(self):
+        for input_line_list, input_line_idx, expected_result in [
+            (["```"], 0, True),
+            (["code"], 0, False),
+            (["```"], 1, False),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_result = p.check_code()
+                self.assertEqual(p._line_idx, input_line_idx)
+                self.assertEqual(actual_result, expected_result)
+
+
+class TestMdlikeParserCheckBody(unittest.TestCase):
+    def test_main(self):
+        for input_line_list, input_line_idx, expected_result in [
+            ([""], 0, False),
+            (["# "], 0, False),
+            (["```"], 0, False),
+            (["body"], 0, True),
+            (["body"], 1, False),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_result = p.check_body()
+                self.assertEqual(p._line_idx, input_line_idx)
+                self.assertEqual(actual_result, expected_result)
+
+
+class TestMdlikeParserSkipBlank(unittest.TestCase):
+    def test_main(self):
+        for input_line_list, input_line_idx, expected_line_idx in [
+            (["a"], 0, 0),
+            (["", "", "a"], 0, 2),
+            ([""], 1, 1),
+            (["", ""], 0, 2),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                p.skip_blank()
+                self.assertEqual(p._line_idx, expected_line_idx)
+
+
+class TestMdlikeParserParseHead(unittest.TestCase):
+    def test_error(self):
+        for input_line_list, input_line_idx in [([""], 0), ([""], 1)]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                with self.assertRaises(ValueError):
+                    p.parse_head()
+                self.assertEqual(p._line_idx, input_line_idx)
+
+    def test_pass(self):
+        for input_line_list, input_line_idx, expected_line_idx, expected_flatelem in [
+            (["# "], 0, 1, sw_luadocs.flatdoc.FlatElem(txt="", kind="head")),
+            (["# a"], 0, 1, sw_luadocs.flatdoc.FlatElem(txt="a", kind="head")),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_flatelem = p.parse_head()
+                self.assertEqual(p._line_idx, expected_line_idx)
+                self.assertEqual(actual_flatelem, expected_flatelem)
+
+
+class TestMdlikeParserParseBody(unittest.TestCase):
+    def test_error(self):
+        for input_line_list, input_line_idx in [([""], 0), ([""], 1)]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                with self.assertRaises(ValueError):
+                    p.parse_body()
+                self.assertEqual(p._line_idx, input_line_idx)
+
+    def test_pass(self):
+        for input_line_list, input_line_idx, expected_line_idx, expected_flatelem in [
+            (
+                ["a", "b", "c"],
+                0,
+                3,
+                sw_luadocs.flatdoc.FlatElem(txt="a\nb\nc", kind="body"),
+            ),
+            (
+                ["a", "b", "c", "", "d", "e", "f"],
+                0,
+                3,
+                sw_luadocs.flatdoc.FlatElem(txt="a\nb\nc", kind="body"),
+            ),
+            (
+                ["a", "b", "c", "# d", "e", "f"],
+                0,
+                3,
+                sw_luadocs.flatdoc.FlatElem(txt="a\nb\nc", kind="body"),
+            ),
+            (
+                ["a", "b", "c", "```", "d", "e", "f", "```"],
+                0,
+                3,
+                sw_luadocs.flatdoc.FlatElem(txt="a\nb\nc", kind="body"),
+            ),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_flatelem = p.parse_body()
+                self.assertEqual(p._line_idx, expected_line_idx)
+                self.assertEqual(actual_flatelem, expected_flatelem)
+
+
+class TestMdlikeParserParseCode(unittest.TestCase):
+    def test_error(self):
+        for input_line_list, input_line_idx in [
+            ([""], 0),
+            ([""], 1),
+            (["```", "a", "b", "c"], 0),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                with self.assertRaises(ValueError):
+                    p.parse_code()
+                self.assertEqual(p._line_idx, input_line_idx)
+
+    def test_pass(self):
+        for input_line_list, input_line_idx, expected_line_idx, expected_flatelem in [
+            (
+                ["```", "```", "d", "e", "f"],
+                0,
+                2,
+                sw_luadocs.flatdoc.FlatElem(txt="", kind="code"),
+            ),
+            (
+                ["```", "a", "b", "c", "```", "d", "e", "f"],
+                0,
+                5,
+                sw_luadocs.flatdoc.FlatElem(txt="a\nb\nc", kind="code"),
+            ),
+            (
+                ["```", "# a", "# b", "# c", "```", "d", "e", "f"],
+                0,
+                5,
+                sw_luadocs.flatdoc.FlatElem(txt="# a\n# b\n# c", kind="code"),
+            ),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_flatelem = p.parse_code()
+                self.assertEqual(p._line_idx, expected_line_idx)
+                self.assertEqual(actual_flatelem, expected_flatelem)
+
+
+class TestMdlikeParserParseElem(unittest.TestCase):
+    def test_error(self):
+        p = sw_luadocs.flatdoc.MdlikeParser("")
+        p._line_list = [""]
+        p._line_idx = 0
+        with self.assertRaises(ValueError):
+            p.parse_elem()
+        self.assertEqual(p._line_idx, 0)
+
+    def test_pass(self):
+        for input_line_list, input_line_idx, expected_line_idx, expected_flatelem in [
+            (
+                ["# a", "# b", "# c"],
+                0,
+                1,
+                sw_luadocs.flatdoc.FlatElem(txt="a", kind="head"),
+            ),
+            (
+                ["a", "b", "c"],
+                0,
+                3,
+                sw_luadocs.flatdoc.FlatElem(txt="a\nb\nc", kind="body"),
+            ),
+            (
+                ["```", "a", "b", "c", "```"],
+                0,
+                5,
+                sw_luadocs.flatdoc.FlatElem(txt="a\nb\nc", kind="code"),
+            ),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list[:]
+                p._line_idx = input_line_idx
+                actual_flatelem = p.parse_elem()
+                self.assertEqual(p._line_idx, expected_line_idx)
+                self.assertEqual(actual_flatelem, expected_flatelem)
+
+
+class TestMdlikeParserParse(unittest.TestCase):
+    def test_error(self):
+        p = sw_luadocs.flatdoc.MdlikeParser("")
+        p._line_list = ["a", "b", "c", "```"]
+        p._line_idx = 0
+        with self.assertRaises(ValueError):
+            p.parse()
+        self.assertEqual(p._line_idx, 0)
+
+    def test_pass(self):
+        for input_line_list, input_line_idx, expected_line_idx, expected_flatdoc in [
+            ([""], 0, 1, []),
+            (
+                ["# a", "b", "c", "d", "```", "e", "f", "g", "```"],
+                0,
+                9,
+                [
+                    sw_luadocs.flatdoc.FlatElem(txt="a", kind="head"),
+                    sw_luadocs.flatdoc.FlatElem(txt="b\nc\nd", kind="body"),
+                    sw_luadocs.flatdoc.FlatElem(txt="e\nf\ng", kind="code"),
+                ],
+            ),
+            (
+                ["", "# a", "", "b", "c", "d", "", "```", "e", "f", "g", "```", ""],
+                0,
+                13,
+                [
+                    sw_luadocs.flatdoc.FlatElem(txt="a", kind="head"),
+                    sw_luadocs.flatdoc.FlatElem(txt="b\nc\nd", kind="body"),
+                    sw_luadocs.flatdoc.FlatElem(txt="e\nf\ng", kind="code"),
+                ],
+            ),
+        ]:
+            with self.subTest(line_list=input_line_list, line_idx=input_line_idx):
+                p = sw_luadocs.flatdoc.MdlikeParser("")
+                p._line_list = input_line_list
+                p._line_idx = input_line_idx
+                actual_flatdoc = p.parse()
+                self.assertEqual(p._line_idx, expected_line_idx)
+                self.assertEqual(actual_flatdoc, expected_flatdoc)
