abf199d14025496229ae09e7d5dfdd30b0243937
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-06-05 14:30:17 +0900

    Remove unused function

--- src/sw_luadocs/flatdoc.py
+++ src/sw_luadocs/flatdoc.py
@@ -33,61 +33,6 @@ def as_flatdoc(v):
     return flatdoc
 
 
-def loads_elem(s):
-    s = str(s)
-    line_list = s.split(sep="\n")
-
-    if not (line_list[0].startswith("[") and line_list[0].endswith("]")):
-        raise ValueError
-    kind = line_list[0][1:-1]
-
-    line_list_end = len(line_list) - 1
-    while line_list_end >= 1:
-        if line_list[line_list_end] != "":
-            break
-        line_list_end -= 1
-    txt = "\n".join(line_list[1 : line_list_end + 1])
-    return FlatElem(txt=txt, kind=kind)
-
-
-def dumps_elem(flatelem):
-    if not isinstance(flatelem, FlatElem):
-        raise TypeError
-
-    s = "[" + flatelem.kind + "]\n" + flatelem.txt
-    s = s.rstrip("\n") + "\n"
-    if loads_elem(s) != flatelem:
-        raise ValueError
-    return s
-
-
-def loads(s):
-    s = str(s)
-    line_list = s.split(sep="\n")
-
-    idx_list = []
-    for idx, line in enumerate(line_list):
-        if line.startswith("[") and line.endswith("]"):
-            idx_list.append(idx)
-    idx_list.append(len(line_list))
-    for line in line_list[: idx_list[0]]:
-        if line != "":
-            raise ValueError
-
-    flatdoc = []
-    for start, stop in itertools.pairwise(idx_list):
-        flatelem = loads_elem("\n".join(line_list[start:stop]))
-        flatdoc.append(flatelem)
-    return flatdoc
-
-
-def dumps(flatdoc):
-    s = "\n".join(map(dumps_elem, flatdoc))
-    if loads(s) != flatdoc:
-        raise ValueError
-    return s
-
-
 class MdlikeParser:
     _linesep = "\n"
     _headseq = "# "
