4b91ba7795efe58e1ba6ee843d4f7835f5ebb3d7
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-06-25 17:12:17 +0900

    Add extract subcommand

--- src/sw_luadocs/main.py
+++ src/sw_luadocs/main.py
@@ -5,6 +5,7 @@ import shlex
 import toml
 
 from . import capture as dot_capture
+from . import extract as dot_extract
 from . import flatdoc as dot_flatdoc
 from . import image as dot_image
 from . import recognize as dot_recognize
@@ -72,46 +73,56 @@ def recognize_main(ns):
         ),
     )
     txt = dot_flatdoc.format_mdlike(flatdoc)
-    with open(ns.txt_file, mode="w", encoding="utf-8", newline="\n") as fobj:
+    with open(ns.recognize_file, mode="w", encoding="utf-8", newline="\n") as fobj:
         fobj.write(txt)
+
+
+def extract_main(ns):
+    cfg = toml.load(ns.config)
+    extract_cfg = cfg["extract"]
+
+    with open(ns.recognize_file, mode="r", encoding="utf-8", newline="\n") as fobj:
+        ocr_txt = fobj.read()
+    with open(ns.stormworks_exe, mode="rb") as fobj:
+        exe_bin = fobj.read()
+
+    ocr_flatdoc = dot_flatdoc.parse_mdlike(ocr_txt)
+    ext_flatdoc = dot_extract.extract(
+        ocr_flatdoc,
+        exe_bin,
+        section_name=extract_cfg["section_name"],
+        body_sep="\n\n",
+        code_sep="\n\n",
+    )
+    ext_txt = dot_flatdoc.format_mdlike(ext_flatdoc)
+
+    with open(ns.extract_file, mode="w", encoding="utf-8", newline="\n") as fobj:
+        fobj.write(ext_txt)
 
 
 def main(*, args=None, exit_on_error=True):
     parser = argparse.ArgumentParser(exit_on_error=exit_on_error)
     parser_group = parser.add_subparsers(required=True, metavar="COMMAND")
 
-    parser_capture = parser_group.add_parser(
-        "capture", help="take screenshots of Stormworks in-game Lua API documentation"
-    )
+    parser_capture = parser_group.add_parser("capture")
     parser_capture.set_defaults(func=capture_main)
-    parser_capture.add_argument(
-        "capture_file", type=pathlib.Path, help="file to save screenshots"
-    )
-    parser_capture.add_argument(
-        "-c", "--config", type=pathlib.Path, required=True, help="configuration file"
-    )
-    parser_capture.add_argument(
-        "--ahk-exe", type=pathlib.Path, help="AutoHotKey executable file"
-    )
+    parser_capture.add_argument("capture_file", type=pathlib.Path)
+    parser_capture.add_argument("-c", "--config", type=pathlib.Path, required=True)
+    parser_capture.add_argument("--ahk-exe", type=pathlib.Path)
 
-    parser_recognize = parser_group.add_parser(
-        "recognize", help="recognizes text in captured screenshots"
-    )
+    parser_recognize = parser_group.add_parser("recognize")
     parser_recognize.set_defaults(func=recognize_main)
-    parser_recognize.add_argument(
-        "capture_file",
-        type=pathlib.Path,
-        help="file of screenshots taken with the capture subcommand",
-    )
-    parser_recognize.add_argument(
-        "txt_file", type=pathlib.Path, help="file to save character recognition results"
-    )
-    parser_recognize.add_argument(
-        "-c", "--config", type=pathlib.Path, required=True, help="configuration file"
-    )
-    parser_recognize.add_argument(
-        "--tesseract-exe", type=pathlib.Path, help="Tesseract OCR executable file"
-    )
+    parser_recognize.add_argument("capture_file", type=pathlib.Path)
+    parser_recognize.add_argument("recognize_file", type=pathlib.Path)
+    parser_recognize.add_argument("-c", "--config", type=pathlib.Path, required=True)
+    parser_recognize.add_argument("--tesseract-exe", type=pathlib.Path)
+
+    parser_extract = parser_group.add_parser("extract")
+    parser_extract.set_defaults(func=extract_main)
+    parser_extract.add_argument("recognize_file", type=pathlib.Path)
+    parser_extract.add_argument("extract_file", type=pathlib.Path)
+    parser_extract.add_argument("-c", "--config", type=pathlib.Path, required=True)
+    parser_extract.add_argument("--stormworks-exe", type=pathlib.Path, required=True)
 
     ns = parser.parse_args(args=args)
     ns.func(ns)
