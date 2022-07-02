f5fe09dbae90ea72334f8d013556ad9a3b576a20
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2022-07-02 21:42:46 +0900

    Use match_flatdoc_each instead of match_flatdoc in sw_luadocs.extract.extract

--- src/sw_luadocs/extract.py
+++ src/sw_luadocs/extract.py
@@ -315,8 +315,8 @@ def match_flatdoc(ocr_flatdoc, ext_txt_set, *, sep="\n\n"):
     return ext_flatdoc, ld_sum
 
 
-def extract(ocr_flatdoc, exe_bin, *, section_name, sep):
+def extract(ocr_flatdoc, exe_bin, *, section_name):
     section_bin = extract_section(exe_bin, section_name, ignore_padding=True)
     ext_txt_set = extract_strings(section_bin)
-    ext_flatdoc, _ = match_flatdoc(ocr_flatdoc, ext_txt_set, sep=sep)
+    ext_flatdoc, _ = match_flatdoc_each(ocr_flatdoc, ext_txt_set)
     return ext_flatdoc
